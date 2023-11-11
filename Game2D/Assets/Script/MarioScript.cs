5fd0e1421737dbb411355c22c7c66cd316b53be8
QuangITcoder <dmquang03032k2@gmail.com>
2023-11-11 09:19:17 +0700

    update 0.5

--- Game2D/Assets/Script/MarioScript.cs
+++ Game2D/Assets/Script/MarioScript.cs
@@ -1,18 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MarioScript : MonoBehaviour
 {
-    public float VanToc;
+    private float VanToc=7;
+    private float VanTocToiDa = 14f; //Vận tốc tối đa khi giữ phím Z
     private float TocDo=0; //toc do
     private bool DuoiDat=true;//kiem tra duoi dat
-    public float NhayCao; //get toc do nhay nv
+    private float NhayCao=500; //get toc do nhay nv
+    private float NhayThap=5; //Áp dụng khi mario nhảy thấp
+    private float RoiXuong=5; //Luc xuống cho mario
     private bool ChuyenHuong=false;//kiemtrachuyenhuong
     private bool QuayPhai = true;//kiem tra nv quay huong nao
+    private float KTGiuPhim = 0.2f;
+    private float TGGiuPhim = 0;
 
     private Rigidbody2D r2d;
     private Animator HoatHoa;
+
+    //Hiện thị cấp độ lớn của Mario
+    public int CapDo = 0;
+    public bool BienHinh = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +37,33 @@ public class MarioScript : MonoBehaviour
         HoatHoa.SetBool("DuoiDat", DuoiDat);
         HoatHoa.SetBool("ChuyenHuong", ChuyenHuong);
         NhayLen();
+        BanDanVaTangToc();
+        //if(BienHinh == true)
+        //{
+        //    switch(CapDo) 
+        //    { 
+        //        case 0:
+        //            {
+        //                StartCoroutine(MarioThuNho());
+        //                BienHinh = false;
+        //                break;
+        //            }
+        //        case 1:
+        //            {
+        //                StartCoroutine(MarioAnNam());
+        //                BienHinh = false;
+        //                break;
+        //            }
+        //        case 2:
+        //            {
+        //                StartCoroutine(MarioAnHoa());
+        //                BienHinh = false;
+        //                break;
+        //            }
+        //        default:BienHinh = false;
+        //                break;
+        //    }
+        //}
     }
     private void FixedUpdate()
     {
@@ -48,6 +84,7 @@ public class MarioScript : MonoBehaviour
         Vector2 HuongQuay = transform.localScale;
         HuongQuay.x *= -1;
         transform.localScale = HuongQuay;
+        if (TocDo > 1) StartCoroutine(MarioChuyenHuong());
 
     }
     void NhayLen()
@@ -55,6 +92,180 @@ public class MarioScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.X)&&DuoiDat==true)
         {
             r2d.AddForce((Vector2.up) * NhayCao);
+            DuoiDat = false;
+        }
+        // áp dụng lực hút trái đất cho mario - rơi xuống nhanh hơn 
+        if (r2d.velocity.y < 0)
+        {
+            r2d.velocity += Vector2.up * Physics2D.gravity.y*(RoiXuong -1)* Time.deltaTime;
+        }
+        else if (r2d.velocity.y>0 && !Input.GetKey(KeyCode.X))
+        {
+            r2d.velocity += Vector2.up * Physics2D.gravity.y * (NhayThap - 1) * Time.deltaTime;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "NenDat")
+        {
+            DuoiDat = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.tag =="NenDat")
+        {
+            DuoiDat = true;
+        }    
+    }
+
+    IEnumerator MarioChuyenHuong()
+    {
+        ChuyenHuong = true;
+        yield return new WaitForSeconds(0.2f);
+        ChuyenHuong = false;
+    }
+
+    //Bắn đạn và chạy nhanh hơn 
+    void BanDanVaTangToc()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            TGGiuPhim += Time.deltaTime;
+            if (TGGiuPhim < KTGiuPhim)
+            {
+                print("Đang bắn đạn chíu chíu");
+            }
+            else
+            {
+                VanToc = VanToc * 1.1f;
+                if (VanToc > VanTocToiDa)
+                    VanToc = VanTocToiDa;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            VanToc = 7f;
+            TGGiuPhim = 0;
+        }
+    }
+
+    //Thay đổi lớn của Mario
+    //IEnumerator MarioAnNam()
+    //{
+    //    float DoTre = 0.1f;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+    //}
+
+    //IEnumerator MarioAnHoa()
+    //{
+    //    float DoTre = 0.1f;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 1;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 1;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 1;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 1;
+    //    yield return new WaitForSeconds(DoTre);
+    //}
+
+    //IEnumerator MarioThuNho()
+    //{
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioNho"), 1;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("MarioLon"), 0;
+    //    HoatHoa.SetLayerWeight(HoatHoa.("AnHoa"), 0;
+    //    yield return new WaitForSeconds(DoTre);
+    //}
 }
